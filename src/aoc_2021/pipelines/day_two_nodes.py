eab3f799f21bdb035eb3bc08d17978bb278ebda9
Jack Roberts <jackroberts360@gmail.com>
2021-12-21 14:44:44 -0600

    day two part 1 of aoc

--- src/aoc_2021/pipelines/day_two_nodes.py
+++ src/aoc_2021/pipelines/day_two_nodes.py
@@ -1,3 +1,4 @@
+from typing import List
 from kedro.pipeline import node
 import pandas as pd
 
@@ -5,23 +6,65 @@ import pandas as pd
 nodes = []
 
 def clean_day2_data(data: str) -> pd.DataFrame:
-    for rows in data:
-        rows = [x.strip().split() for x in rows if x.strip()]    
-    return pd.DataFrame(rows)
+    new_data = [x for x in data.split("\n") if x]
+    _df = pd.DataFrame(new_data)
+    _df[[0,1]] = _df[0].str.split(' ',expand=True)
+    return _df
     
 
 def find_position(day_two: pd.DataFrame) -> str:
-    """count_increases.
+    """find_position.
 
     Args:
-        day_one (pd.DataFrame): day_one sonar measurements
+        day_two (pd.DataFrame): day_two movements
 
     Returns:
-        str: count of increases as STRING becuase STUPID
+        str: final position as STRING
     """
-    diffs = day_two.diff()
-    res: pd.Series = diffs[diffs > 0].count()
-    return str(res.values[0])
+    forward_position = get_sum_of_distance(filter_dataframe_by_direction(day_two, "forward"))
+    up_position = get_sum_of_distance(filter_dataframe_by_direction(day_two, "up"))
+    down_position = get_sum_of_distance(filter_dataframe_by_direction(day_two, "down"))
+    return calculate_final_position(forward_position, up_position, down_position)
+    
+    
+def calculate_final_position(forward: int, up: int, down: int) -> int:
+    """find_position.
+
+    Args:
+        forward (int): forward distance traveled
+        up (int): up distance traveled
+        down (int): down distance traveled
+
+    Returns:
+        int: number representig final position
+    """
+    return abs((up - down)) * forward
+    
+
+
+def filter_dataframe_by_direction(day_two: pd.DataFrame, direction: str) -> pd.DataFrame:
+    """filter_dataframe_by_direction.
+
+    Args:
+        day_two (pd.DataFrame): day_two movements
+        direction (Str): direction to be filtered by
+
+    Returns:
+        pd.DataFrame: filterd by direction
+    """
+    return day_two[day_two[0].eq(direction)]
+
+
+def get_sum_of_distance(df: pd.DataFrame) -> int:
+    """get_sum_of_distance.
+
+    Args:
+        df (pd.DataFrame): movements filtered by direction
+
+    Returns:
+        int: total distance traveled in particular direction
+    """
+    return df[1].astype(int).sum()
 
 
 nodes.append(
